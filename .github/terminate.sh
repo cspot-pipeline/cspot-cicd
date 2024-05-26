f3a2d033c2d0299a46af8627d3bd0c49e282d63d
obenedek20 <97198603+obenedek20@users.noreply.github.com>
2024-05-25 17:20:22 -0700

    Add files via upload

---
+++ .github/terminate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Options:
+#  -i   instance id
+
+set -e
+
+while getopts "k:n:p:" opt; do
+  case "${opt}" in
+    i) INSTANCE_ID=${OPTARG}
+      ;;
+    *) exit
+      ;;
+  esac
+done
+
+SCRIPT_PATH=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+REGION="eucalyptus"
+
+#chmod 400 "${KEYFILE}"
+
+function euca() {
+   command aws ${AWS_PROFILE:-'--endpoint-url' 'http://ec2.poc.aristotle.ucsb.edu:443'} \
+      --region "${REGION}" --no-paginate --no-verify-ssl --output text "$@"
+}
+
+echo 'Terminating instance...'
+EC2_INSTANCE="$(euca ec2 terminate-instances \
+                    --instance-ids "${INSTANCE_ID}" \
+                | grep INSTANCES \
+                | awk -F' ' '{ print $6; }')"
+
+echo "Terminated instance with ID ${INSTANCE_ID}."
