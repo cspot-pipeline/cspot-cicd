1404633275bc898da2e755337a6c61ff7a174277
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2024-05-28 14:06:45 -0700

    fix up workflow files into 1 YAML

--- .github/cspot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/cspot-pipeline/cspot-cicd.git
-cd cspot-cicd || { echo "Directory cspot-cicd does not exist"; exit 1; }
-cd build || { echo "Directory build does not exist"; exit 1; }
-sudo ninja install
