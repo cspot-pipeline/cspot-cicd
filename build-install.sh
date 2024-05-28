bcb7ac4011dff537d83268a44d2f827177681989
obenedek20 <97198603+obenedek20@users.noreply.github.com>
2024-05-28 13:51:02 -0700

    Update build-install.sh

--- build-install.sh
+++ build-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git submodule update --init --recursive
 mv deps/libzmq/CMakeLists.txt deps/libzmq/CMakeLists.orig.txt
 sed 's/build the tests" ON/build the tests" OFF/' deps/libzmq/CMakeLists.orig.txt > deps/libzmq/CMakeLists.txt
