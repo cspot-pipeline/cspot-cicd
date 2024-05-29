e012849adaeca843141a986f013548c575e04c4d
obenedek20 <97198603+obenedek20@users.noreply.github.com>
2024-05-29 13:54:34 -0700

    Update add_runner.sh

--- .github/add_runner.sh
+++ .github/add_runner.sh
@@ -4,7 +4,7 @@ curr_runners=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $ADD_RUNNER" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/cspot-pipeline/cspot-cicd/actions/runners)
+  https://api.github.com/repos/cspot-pipeline/cspot-cicd/actions/runners)
   
 num_runners=$(echo "$curr_runners" | jq -r '.total_count')
 runner_name="cspot-runner-$num_runners"
@@ -17,7 +17,7 @@ json_response=$(
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $ADD_RUNNER" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
-      https://api.github.com/orgs/cspot-pipeline/cspot-cicd/actions/runners/generate-jitconfig \
+      https://api.github.com/repos/cspot-pipeline/cspot-cicd/actions/runners/generate-jitconfig \
       -d "{\"name\":\"$runner_name\",\"runner_group_id\":1, \"labels\":[\"self-hosted\",\"X64\",\"Linux\"]}")
 
 # Print the JSON response to verify
