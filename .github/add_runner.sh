9a399384f70c5c8dadde77e26f692001322b0cd2
obenedek20 <97198603+obenedek20@users.noreply.github.com>
2024-05-29 12:47:13 -0700

    Update add_runner.sh

--- .github/add_runner.sh
+++ .github/add_runner.sh
@@ -11,7 +11,7 @@ json_response=$(
       -H "Authorization: Bearer $GITHUB_TOKEN}" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       https://api.github.com/repos/cspot-pipeline/cspot-cicd/actions/runners/generate-jitconfig \
-      -d '{"name":"cspot-runner","labels":["self-hosted","X64","Linux"]')
+      -d '{"name":"cspot-runner", "runner_group_id": 1, "labels":["self-hosted","X64","Linux"], "work_folder": "_work"')
 
 # Print the JSON response to verify
 echo "$json_response"
