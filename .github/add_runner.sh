6192e1646ca56f3f8cb8040fc9fae834e4160ddb
obenedek20 <97198603+obenedek20@users.noreply.github.com>
2024-05-29 13:33:56 -0700

    Update add_runner.sh

--- .github/add_runner.sh
+++ .github/add_runner.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Check github token
-echo $ADD_RUNNER
-
 # Perform the curl call and store the JSON response in a variable
 json_response=$(
     curl -L \
       -X POST \
       -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer $ADD_RUNNER}" \
+      -H "Authorization: Bearer $ADD_RUNNER" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
-      https://api.github.com/orgs/cspot-pipeline/cspot-cicd/actions/runners/generate-jitconfig \
-      -d '{"name":"cspot-runner","labels":["self-hosted","X64","Linux"]')
+      https://api.github.com/orgs/cspot-pipeline/actions/runners/generate-jitconfig \
+      -d '{"name":"cspot-runner","runner_group_id":1, "labels":["self-hosted","X64","Linux"]')
 
 # Print the JSON response to verify
 echo "$json_response"
