5e6687d28a709a66207f3203b6c8ec6d7585219e
Ryan Wenger <59625987+rawenger@users.noreply.github.com>
2024-06-03 18:51:47 -0700

    Configure instances to terminate on shutdown

--- .github/launch-runner.py
+++ .github/launch-runner.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
 
 	print('Creating instance...')
 	try:
-		create_output = aws('ec2', 'run-instances', \
-						'--key-name', args.key_name, \
-						'--image-id', AMI_IMAGE, \
+		create_output = aws('ec2', 'run-instances',
+						'--key-name', args.key_name,
+						'--image-id', AMI_IMAGE,
 						'--instance-type', 't2.xlarge')
 	except CalledProcessError as error:
 		print(error.output)
@@ -34,6 +34,10 @@ if __name__ == "__main__":
 	ec2_instance = json.loads(create_output)['Instances'][0]['InstanceId']
 	public_ip = json.loads(create_output)['Instances'][0]['PublicIpAddress']
 	print(f'Created new instance with ID {ec2_instance} at public IP {public_ip}.')
+
+	aws('ec2', 'modify-instance-attribute',
+	    '--instance-id', ec2_instance,
+	    '--instance-initiated-shutdown-behavior', '{"Value": "Terminate"}')
 
 	while True:
 		print('Checking if instance is live...', end='')
